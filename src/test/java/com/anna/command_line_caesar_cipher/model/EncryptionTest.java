5d5526177d146e5571f567cc314911539dceac84
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-24 17:07:04 +0300

    Add test to check for correct instantiation of Encryption class

--- src/test/java/com/anna/command_line_caesar_cipher/model/EncryptionTest.java
+++ src/test/java/com/anna/command_line_caesar_cipher/model/EncryptionTest.java
@@ -1,7 +1,18 @@
 package com.anna.command_line_caesar_cipher.model;
 
+import com.anna.command_line_caesar_cipher.model.parameter_resolver.EncryptionParameterResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(EncryptionParameterResolver.class)
 class EncryptionTest {
-
+  @Test
+  @DisplayName("Test for correct instantiation of Encryption class")
+  public void newEncryption_instantiatesCorrectly() {
+    Encryption encryption = new Encryption("Hello", 1);
+    assertTrue(encryption instanceof Encryption);
+  }
 }
