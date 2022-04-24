9fe6be03e6e31038276eb588647beb2a4b623301
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-24 19:32:42 +0300

    Add implementation of getter method

--- src/main/java/com/anna/command_line_caesar_cipher/model/Decryption.java
+++ src/main/java/com/anna/command_line_caesar_cipher/model/Decryption.java
@@ -1,6 +1,12 @@
 package com.anna.command_line_caesar_cipher.model;
 
 public class Decryption {
+  private String input;
   public Decryption(String input, int key) {
+    this.input = input;
+  }
+
+  public String getInput(){
+    return this.input;
   }
 }
