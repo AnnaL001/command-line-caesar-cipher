096941a82f3461a19d30a79ac9101472728e4152
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-24 19:21:12 +0300

    Add test to check for correct instantiation of Decryption class

--- src/test/java/com/anna/command_line_caesar_cipher/model/DecryptionTest.java
+++ src/test/java/com/anna/command_line_caesar_cipher/model/DecryptionTest.java
@@ -1,7 +1,16 @@
 package com.anna.command_line_caesar_cipher.model;
 
+import com.anna.command_line_caesar_cipher.model.parameter_resolver.DecryptionParameterResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(DecryptionParameterResolver.class)
 class DecryptionTest {
-
+  @Test
+  @DisplayName("Test for correct instantiation of Decryption class")
+  public void newDecryption_instantiatesCorrectly(Decryption decryption) {
+    assertNotNull(decryption);
+  }
 }
