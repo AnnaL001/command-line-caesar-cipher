8821a039565db809849a0096b6be99b5368fdd4d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-24 18:36:41 +0300

    Add implementation code for corresponding test to pass

--- src/main/java/com/anna/command_line_caesar_cipher/model/Encryption.java
+++ src/main/java/com/anna/command_line_caesar_cipher/model/Encryption.java
@@ -2,12 +2,17 @@ package com.anna.command_line_caesar_cipher.model;
 
 public class Encryption {
   private String input;
+  private int key;
   public Encryption(String input, int key) {
     this.input = input;
+    this.key = key;
   }
 
   public String getInput(){
     return this.input;
   }
 
+  public int getKey(){
+    return this.key;
+  }
 }
